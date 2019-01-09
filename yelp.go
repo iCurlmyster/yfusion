aa040622a9514d79bedc163deff40802bc090006
Joshua Matthews <jmatth93@gmail.com>
2019-01-08 22:08:07 -0600

    added context params to requests

--- yelp.go
+++ yelp.go
@@ -1,6 +1,7 @@
 package yfusion
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"net/http"
@@ -39,19 +40,38 @@ func NewYelpFusionWithClient(key string, client *http.Client) *YelpFusion {
 	}
 }
 
-func (yf *YelpFusion) getRequest(method, url string) (*http.Request, error) {
+func (yf *YelpFusion) getRequest(ctx context.Context, method, url string) (*http.Request, error) {
 	req, err := http.NewRequest(method, url, nil)
 	if err != nil {
 		return nil, err
 	}
 	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", yf.apiKey))
+	if ctx != nil {
+		req.WithContext(ctx)
+	}
 	return req, nil
 }
 
 // SearchBusiness - Use the Business Search route with the given BussinesSearchParams options
 // returns the parsed BusinessSearchData object
 func (yf *YelpFusion) SearchBusiness(bus *BusinessSearchParams) (*BusinessSearchData, error) {
-	resp, err := yf.SearchBusinessResponse(bus)
+	resp, err := yf.SearchBusinessResponse(nil, bus)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	var b *BusinessSearchData
+	decode := json.NewDecoder(resp.Body)
+	if err := decode.Decode(&b); err != nil {
+		return nil, err
+	}
+	return b, nil
+}
+
+// SearchBusinessWithContext - Use the Business Search route with the given BussinesSearchParams options and context
+// returns the parsed BusinessSearchData object
+func (yf *YelpFusion) SearchBusinessWithContext(ctx context.Context, bus *BusinessSearchParams) (*BusinessSearchData, error) {
+	resp, err := yf.SearchBusinessResponse(ctx, bus)
 	if err != nil {
 		return nil, err
 	}
@@ -66,13 +86,13 @@ func (yf *YelpFusion) SearchBusiness(bus *BusinessSearchParams) (*BusinessSearch
 
 // SearchBusinessResponse - Use the Business Search route with the given BusinessSearchParams options
 // returns the Response from the request
-func (yf *YelpFusion) SearchBusinessResponse(bus *BusinessSearchParams) (*http.Response, error) {
+func (yf *YelpFusion) SearchBusinessResponse(ctx context.Context, bus *BusinessSearchParams) (*http.Response, error) {
 	params, err := bus.Params()
 	if err != nil {
 		return nil, err
 	}
 	urlStr := fmt.Sprintf("%s%s?%s", baseURL, busSearch, params)
-	req, err := yf.getRequest("GET", urlStr)
+	req, err := yf.getRequest(ctx, "GET", urlStr)
 	if err != nil {
 		return nil, err
 	}
@@ -82,14 +102,14 @@ func (yf *YelpFusion) SearchBusinessResponse(bus *BusinessSearchParams) (*http.R
 // SearchBusinessDetails - Query details about a business, given its ID
 // returns the parsed DetailedBusinessInfo object
 func (yf *YelpFusion) SearchBusinessDetails(busID string) (*DetailedBusinessInfo, error) {
-	return yf.SearchBusinessDetailsWithLocale(busID, "")
+	return yf.SearchBusinessDetailsWithLocale(nil, busID, "")
 }
 
-// SearchBusinessDetailsWithLocale - Query details about a business, given its ID.
+// SearchBusinessDetailsWithLocale - Query details about a business, given its ID with context.
 // With the option of specifing a locale. (An empty string for locale will leave the parameter off)
 // returns the parsed DetailedBusinessInfo object
-func (yf *YelpFusion) SearchBusinessDetailsWithLocale(busID, locale string) (*DetailedBusinessInfo, error) {
-	resp, err := yf.SearchBusinessDetailsWithLocaleResponse(busID, locale)
+func (yf *YelpFusion) SearchBusinessDetailsWithLocale(ctx context.Context, busID, locale string) (*DetailedBusinessInfo, error) {
+	resp, err := yf.SearchBusinessDetailsWithLocaleResponse(ctx, busID, locale)
 	if err != nil {
 		return nil, err
 	}
@@ -104,12 +124,12 @@ func (yf *YelpFusion) SearchBusinessDetailsWithLocale(busID, locale string) (*De
 // SearchBusinessDetailsWithLocaleResponse - Query details about a business, given its ID.
 // With the option of specifing a locale. (An empty string for locale will leave the parameter off)
 // returns the Response from the request
-func (yf *YelpFusion) SearchBusinessDetailsWithLocaleResponse(busID, locale string) (*http.Response, error) {
+func (yf *YelpFusion) SearchBusinessDetailsWithLocaleResponse(ctx context.Context, busID, locale string) (*http.Response, error) {
 	urlStr := fmt.Sprintf("%s%s/%s", baseURL, busDetails, busID)
 	if strings.TrimSpace(locale) != "" {
 		urlStr = fmt.Sprintf("%s?locale=%s", urlStr, url.QueryEscape(locale))
 	}
-	req, err := yf.getRequest("GET", urlStr)
+	req, err := yf.getRequest(ctx, "GET", urlStr)
 	if err != nil {
 		return nil, err
 	}
